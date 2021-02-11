1c2b19c85b3e82f3412c5d67dc2bb37de687fccd
Michael Whalen <michael@whalesalad.com>
2021-02-11 15:01:19 -0500

    Eliminate cache concept, Python 2 support, simplify.

--- whois/_1_query.py
+++ whois/_1_query.py
@@ -1,65 +1,33 @@
 import subprocess
-import time
-import sys
-import os
-from .exceptions import WhoisCommandFailed
+
+from .exceptions import WHOISCommandFailed, WHOISCommandTimeout
 
 
-PYTHON_VERSION = sys.version_info[0]
-CACHE = {}
-CACHE_MAX_AGE = 60*60*48    # 48h
-
-try:
-    import json
-except:
-    import simplejson as json
+DEFAULT_TIMEOUT = 10
 
 
-def cache_load(cf):
-    if not os.path.isfile(cf):
-        return
+def do_query(dl, ignore_returncode=False, timeout=None):
+    """
+    Linux 'whois' command wrapper
 
-    global CACHE
-    f = open(cf, 'r')
+    """
+    cmd = ['whois', dl]
+
+    p = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        env={"LANG": "ja"}
+    )
 
     try:
-        CACHE = json.load(f)
-    except:
-        pass
-    f.close()
+        r = p.communicate(timeout=timeout or DEFAULT_TIMEOUT)[0]
+    except subprocess.TimeoutExpired as e:
+        raise WHOISCommandTimeout('Timeout') from e
 
+    r = r.decode()
 
-def cache_save(cf):
-    global CACHE
-    f = open(cf, 'w')
-    json.dump(CACHE, f)
-    f.close()
-
-
-def do_query(dl, force=0, cache_file=None, slow_down=0, ignore_returncode=0):
-    k = '.'.join(dl)
-    if cache_file:
-        cache_load(cache_file)
-    if force or k not in CACHE or CACHE[k][0] < time.time() - CACHE_MAX_AGE:
-        CACHE[k] = (
-            int(time.time()),
-            _do_whois_query(dl, ignore_returncode),
-        )
-        if cache_file:
-            cache_save(cache_file)
-        if slow_down:
-            time.sleep(slow_down)
-
-    return CACHE[k][1]
-
-
-def _do_whois_query(dl, ignore_returncode):
-    """
-        Linux 'whois' command wrapper
-    """
-    p = subprocess.Popen(['whois', '.'.join(dl)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env={"LANG": "ja"})
-    r = p.communicate()[0]
-    r = r.decode() if PYTHON_VERSION == 3 else r
     if not ignore_returncode and p.returncode != 0 and p.returncode != 1:
-        raise WhoisCommandFailed(r)
+        raise WHOISCommandFailed(r)
+
     return r
